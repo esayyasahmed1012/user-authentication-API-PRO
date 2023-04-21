7a9fb8e12718cc37f8d6f3265a5e7390df59a586
esayyas <esayyasahmed1012@gmail.com>
2023-04-21 16:52:46 +0300

    country.py

--- flask_auth/models/country.py
+++ flask_auth/models/country.py
@@ -1,6 +1,6 @@
 """Country model."""
-from flaskauth.models.base_model import BaseModel, db
-from flaskauth.models.region import Region
+from flask_auth.models.base_model import BaseModel, db
+from flask_auth.models.region import Region
 
 class Country(db.Model, BaseModel):
 	__tablename__ = "countries"
